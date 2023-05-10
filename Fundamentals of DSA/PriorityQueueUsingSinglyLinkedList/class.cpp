688aa2961b6476482a01e44c6f59f1e2a49b94b0
Lovish Duggal <lovishduggal11@gmail.com>
2023-05-10 19:37:01 +0530

    insertInPQ

--- Fundamentals of DSA/PriorityQueueUsingSinglyLinkedList/class.cpp
+++ Fundamentals of DSA/PriorityQueueUsingSinglyLinkedList/class.cpp
@@ -12,7 +12,45 @@ private:
     node *start;
 
 public:
+    PriorityQueue();
+    void insert(int, int);
 };
+void PriorityQueue::insert(int priority, int data)
+{
+    node *n = new node;
+    n->item = data;
+    n->priorityNo = priority;
+    if (start == NULL)
+    {
+        n->next = NULL;
+        start = n;
+    }
+    else
+    {
+        if (n->priorityNo > start->priorityNo)
+        {
+            n->next = start;
+            start = n;
+        }
+        else
+        {
+            node *t = start;
+            while (t->next != NULL)
+            {
+                if (t->next->priorityNo > n->priorityNo)
+                    t = t->next;
+                else
+                    break;
+            }
+            n->next = t->next;
+            t->next = n;
+        }
+    }
+}
+PriorityQueue::PriorityQueue()
+{
+    start = NULL;
+}
 int main()
 {
 }
